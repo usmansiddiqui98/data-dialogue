584806220334a63c6d7945dc20f7148c2d6389a6
Usman Siddiqui <usmanhasansiddiqui@gmail.com>
2023-02-10 18:25:55 +0800

    add template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Whether for a B2B or B2C company, relevance and longevity in the industry depends on how well the products answer the needs of the customers. However, when the time comes for the companies to demonstrate that understanding — during a sales conversation, customer service interaction, or through the product itself — how can companies evaluate how they measure up?',
+    author='Usman Siddiqui',
+    license='MIT',
+)
