eced9f1d4ac3f19c79a49e10b9b453c0c98505c5
jialin-low <73953587+jialin-low@users.noreply.github.com>
2023-03-21 20:30:51 +0800

    Revert "Revert "Merge branch 'master' into nmf""
    This reverts commit 8447e0011105a6b4e4b5664c2214b94adfc7300a.

--- src/app/_pages/main.py
+++ src/app/_pages/main.py
@@ -2,11 +2,13 @@ import streamlit as st
 
 import src.app._pages.home as home
 import src.app._pages.sentiment_analysis.main as sentiment_analysis
+import src.app._pages.topic_modelling.main as topic_modelling
 import src.app.navigation as navigation
 
 router = {
     "Home": home,
     "Sentiment Analysis": sentiment_analysis,
+    "Topic Modelling": topic_modelling,
 }
 
 
