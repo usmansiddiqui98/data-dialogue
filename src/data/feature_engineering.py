61e7053d49ab97fe8fab3ae1c0f928b9c16adac1
chae-seong <chaeseong123@hotmail.com>
2023-03-22 18:56:48 +0800

    Formatting

--- src/data/feature_engineering.py
+++ src/data/feature_engineering.py
@@ -1,15 +1,16 @@
-import pandas as pd
 import string
 from nltk.tokenize import word_tokenize
+
 
 # Define function to count number of lowercase
 def count_lower(sentence):
     words = word_tokenize(sentence)
     count = 0
     for word in words:
-        if not word.isupper(): # eg Real is considered lowercase
+        if not word.isupper():  # eg Real is considered lowercase
             count += 1
     return count
+
 
 # Define function to count number of uppercase
 def count_upper(sentence):
@@ -17,9 +18,10 @@ def count_upper(sentence):
     count = 0
     for word in words:
         if word.isupper():
-            if len(word) > 1: # exclude one letter words eg 'I'
+            if len(word) > 1:  # exclude one letter words eg 'I'
                 count += 1
     return count
+
 
 # Define function to list uppercase words
 def uppercase_list(sentence):
@@ -27,10 +29,11 @@ def uppercase_list(sentence):
     uppercase = []
     for word in words:
         if word.isupper():
-            if len(word) > 1: # exclude one letter words eg 'I'
+            if len(word) > 1:  # exclude one letter words eg 'I'
                 uppercase.append(word)
     uppercase = ", ".join(uppercase)
     return uppercase
+
 
 # Define function to get uppercase:total tokens ratio
 def uppercase_ratio(sentence):
@@ -40,8 +43,9 @@ def uppercase_ratio(sentence):
         if word.isupper():
             if len(word) > 1:  # exclude 'I'
                 count += 1
-    ratio = count/len(words)
+    ratio = count / len(words)
     return ratio
+
 
 # Define function to count number of punctuations
 def count_punc(sentence):
@@ -55,9 +59,9 @@ def count_punc(sentence):
 
 def add_features(df):
     new_df = df.copy()
-    new_df['Lowercase Count'] = new_df['text'].apply(count_lower)
-    new_df['Uppercase Count'] = new_df['text'].apply(count_upper)
-    new_df['Uppercase Words'] = new_df['text'].apply(uppercase_list)
-    new_df['Uppercase Ratio'] = new_df['text'].apply(uppercase_ratio)
-    new_df['Punc Count'] = new_df['text'].apply(count_punc)
+    new_df["Lowercase Count"] = new_df["text"].apply(count_lower)
+    new_df["Uppercase Count"] = new_df["text"].apply(count_upper)
+    new_df["Uppercase Words"] = new_df["text"].apply(uppercase_list)
+    new_df["Uppercase Ratio"] = new_df["text"].apply(uppercase_ratio)
+    new_df["Punc Count"] = new_df["text"].apply(count_punc)
     return new_df
