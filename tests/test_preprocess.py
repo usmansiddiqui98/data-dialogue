c23a3f61f65f960217d6b43e57a4b021acc8990e
tan-yx <tanyingxian123@gmail.com>
2023-03-24 17:46:14 +0800

    fixed tests to adhere to new code structure

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,33 +1,24 @@
 import os
 
-import pandas as pd
-
-import src.data.preprocess as preprocess
+from src.data.preprocess import Preprocessor
 
 fname = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data", "raw", "reviews.csv"))
 
 
 def test_clean():
-    text = preprocess.clean(
+    text = Preprocessor.clean_sentence(
         "This is a very healthy dog food. Good for their digestion. Also good for small puppies. "
         "My dog eats her required amount at every feeding."
     )
     assert text == "healthy dog food good digestion also good small puppy dog eats require amount every feeding"
 
 
-def test_clean_df():
-    df = pd.read_csv(fname)
-    df = preprocess.clean_df(df)
-    assert (
-        df["Cleaned Text"][5] == "like people mention coffee great taste try different instant coffee one one "
-        "good one another one favorites want try though organic pure instant coffee"
-    )
-
-
 def test_clean_csv():
-    df = preprocess.clean_csv(fname)
+    preprocessor = Preprocessor(fname)
+    preprocessor.clean_csv()
+    df = preprocessor.clean_df
     assert (
-        df["Cleaned Text"][1] == "pleased natural balance dog food dog issue dog food past someone recommend "
+        df["cleaned_text"][1] == "pleased natural balance dog food dog issue dog food past someone recommend "
         "natural balance grain free since possible allergic grain since switch issue "
         "also helpful different kibble size size dog"
     )
