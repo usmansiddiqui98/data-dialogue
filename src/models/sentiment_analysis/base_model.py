269a15ea7f13e640ffcad7410933a6d116a8f195
usmansiddiqui98 <usmanhasansiddiqui@gmail.com>
2023-03-29 03:38:02 +0800

    add evaluation for basemodel

--- src/models/sentiment_analysis/base_model.py
+++ src/models/sentiment_analysis/base_model.py
@@ -1,3 +1,8 @@
+import numpy as np
+import torch
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+
+
 class BaseModel:
     def __init__(self):
         self.model = None
@@ -7,3 +12,10 @@ class BaseModel:
 
     def predict(self, X_test):
         return self.model.predict(X_test)
+
+    def evaluate(self, X_test, y_test):
+        f1 = f1_score(y_test, self.predict(X_test), average="weighted")
+        precision = precision_score(y_test, self.predict(X_test), average="weighted")
+        recall = recall_score(y_test, self.predict(X_test), average="weighted")
+        accuracy = accuracy_score(y_test, self.predict(X_test))
+        return f1, precision, recall, accuracy
