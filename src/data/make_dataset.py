40f88fc870b159197d264ddf81b5735214313909
usmansiddiqui98 <usmanhasansiddiqui@gmail.com>
2023-03-29 02:57:08 +0800

    update make_dataset to return x and y train+test sets

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -18,25 +18,30 @@ def main(input_filepath, train_split_output_filepath, test_split_output_filepath
     feature_engineer = FeatureEngineer(pre_processed_df)
     feature_engineer.add_features()
     feature_engineered_df = feature_engineer.feature_engineered_df
+    # Separate target variable (y) and features (X)
+    X = feature_engineered_df.drop("sentiment", axis=1)
+    y = feature_engineered_df["sentiment"]
     # train test split and write splits to csv
-    train, test = train_test_split(
-        feature_engineered_df, test_size=0.2, random_state=4263, stratify=feature_engineered_df["sentiment"]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=4263, stratify=feature_engineered_df["sentiment"]
     )
 
-    train.to_csv(train_split_output_filepath)
-    test.to_csv(test_split_output_filepath)
+    # Write splits to csv
+    # train = X_train.join(y_train)
+    # test = X_test.join(y_test)
+    # train.to_csv(train_split_output_filepath)
+    # test.to_csv(test_split_output_filepath)
+
+    return X_train, X_test, y_train, y_test
 
 
 if __name__ == "__main__":
     log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
     # relative dir
     input_file = "../../data/raw/reviews.csv"
     train_output_file = "../../data/processed/train_final_processed_reviews.csv"
     test_output_file = "../../data/processed/test_final_processed_reviews.csv"
-    main(input_file, train_output_file, test_output_file)
+    X_train, X_test, y_train, y_test = main(input_file, train_output_file, test_output_file)
