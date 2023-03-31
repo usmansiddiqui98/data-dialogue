e43c46b85f6a1ce898b50c1440c8f95b4070fbca
tan-yx <tanyingxian123@gmail.com>
2023-03-31 17:50:38 +0800

    added seibert.py

---
+++ src/models/sentiment_analysis/pre_trained/seibert.py
@@ -0,0 +1,20 @@
+from transformers import pipeline
+import torch
+import pandas as pd
+
+
+class Seibert:
+    def __init__(self):
+        if torch.cuda.is_available():
+            self.device = 0
+        else:
+            self.device = -1
+
+    def predict(self, x_test):
+        x_test = x_test.text.to_list()
+        x_test = [x[:512] if len(x) > 512 else x for x in x_test]
+        specific_model = pipeline(model="siebert/sentiment-roberta-large-english", device=self.device)
+        results = specific_model(x_test)
+        labels = [result['label'] for result in results]
+        y_pred = [1 if label == 'POSITIVE' else 0 for label in labels]
+        return y_pred
