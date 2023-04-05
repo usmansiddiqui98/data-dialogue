576a63af6593d86655df35d1cce51988a45ad46a
usmansiddiqui98 <usmanhasansiddiqui@gmail.com>
2023-04-06 07:09:02 +0800

    trained xgboost vanilla

---
+++ src/models/sentiment_analysis/xg_boost.py
@@ -0,0 +1,14 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from xgboost import XGBClassifier
+
+from src.models.sentiment_analysis.base_model import BaseModel
+
+
+class XgBoost(BaseModel):
+    def __init__(self, vectorizer, dim_reducer=None, *args, **kwargs):
+        super().__init__(vectorizer=vectorizer, dim_reducer=dim_reducer, *args, **kwargs)
+        self.model = XGBClassifier(use_label_encoder=False, eval_metric='mlogloss')
+
+    def fit(self, X_train, y_train):
+        X_train_processed = self.process(X_train)
+        self.model.fit(X_train_processed, y_train)
