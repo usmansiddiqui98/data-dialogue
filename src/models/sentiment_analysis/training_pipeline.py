24f2c6932c7c03cb879e2dd624e4a45f9c4bb856
tan-yx <tanyingxian123@gmail.com>
2023-04-06 17:53:12 +0800

    refactor

--- src/models/sentiment_analysis/training_pipeline.py
+++ src/models/sentiment_analysis/training_pipeline.py
@@ -1,6 +1,9 @@
 import os
 import pickle
+from sys import platform
 
+import numpy as np
+import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score
 
@@ -17,70 +20,59 @@ def train_models(models, X_train, y_train, models_path):
         model_dir = os.path.join(models_path, model_name)
         os.makedirs(model_dir, exist_ok=True)
 
-        # Save the model, vectorizer, and dimensionality reducer to pickle files
-        with open(os.path.join(model_dir, "model.pkl"), "wb") as f:
-            pickle.dump(model_instance.model, f)
-        with open(os.path.join(model_dir, "vectorizer.pkl"), "wb") as f:
-            pickle.dump(model_instance.vectorizer, f)
-        if model_instance.dim_reducer is not None:
-            with open(os.path.join(model_dir, "dim_reducer.pkl"), "wb") as f:
-                pickle.dump(model_instance.dim_reducer, f)
+        model_instance.save(model_name)
 
 
-def find_best_model(models_path, X_test, y_test):
+def find_best_model(models, models_path, X_test, y_test):
     best_accuracy = 0
     best_model = None
-    best_vectorizer = None
-    best_dim_reducer = None
     best_model_name = None
 
     # Iterate through the model directories
-    for model_name in os.listdir(models_path):
+    for model_name, model_instance in models.items():
         model_dir = os.path.join(models_path, model_name)
-
         if os.path.isdir(model_dir):
-            # Load model, vectorizer, and dimension reducer
-            with open(os.path.join(model_dir, "model.pkl"), "rb") as f:
-                model = pickle.load(f)
-            with open(os.path.join(model_dir, "vectorizer.pkl"), "rb") as f:
-                vectorizer = pickle.load(f)
-            with open(os.path.join(model_dir, "dim_reducer.pkl"), "rb") as f:
-                dim_reducer = pickle.load(f)
-
-            # Preprocess the test data
-            X_test_vectorized = vectorizer.transform(X_test)
-            if dim_reducer is not None:
-                X_test_reduced = dim_reducer.transform(X_test_vectorized)
-            else:
-                X_test_reduced = X_test_vectorized
-
-            # Predict and evaluate the model
-            y_pred = model.predict(X_test_reduced)
+            model_instance.load(model_name)
+            pred = model_instance.predict(X_test)
+            y_pred = pred["predicted_sentiment"]
             accuracy = accuracy_score(y_test, y_pred)
 
             # Check if the model has the highest accuracy
             if accuracy > best_accuracy:
                 best_accuracy = accuracy
-                best_model = model
-                best_vectorizer = vectorizer
-                best_dim_reducer = dim_reducer
+                best_model = model_instance
                 best_model_name = model_name
 
-    return best_model, best_vectorizer, best_dim_reducer, best_model_name, best_accuracy
+    return best_model, best_model_name, best_accuracy
 
 
 if __name__ == "__main__":
     # Load the data
-    X_train, X_test, y_train, y_test = make_dataset("../../../data/raw/reviews.csv")
+
+    # train = pd.read_csv("../../../data/processed/train_final_processed_reviews.csv", index_col="Unnamed: 0")
+    # test = pd.read_csv("../../../data/processed/test_final_processed_reviews.csv", index_col="Unnamed: 0")
+    # X_train = train.drop("sentiment", axis=1)
+    # X_test = test.drop("sentiment", axis=1)
+    # y_train = train.sentiment.tolist()
+    # y_test = test.sentiment.tolist()
+
+    X_train, X_test, y_train, y_test = make_dataset(
+        "../../../data/raw/reviews.csv",
+        train_split_output_filepath="../../../data/processed/train_final_processed_reviews.csv",
+        test_split_output_filepath="../../../data/processed/test_final_processed_reviews.csv",
+    )
+
+    if platform == "win32":
+        models_path = "..\\..\\..\\models\\sentiment_analysis"
+    else:
+        models_path = "../../../models/sentiment_analysis"
     models = {
-        "xg_boost": XgBoost(vectorizer=TfidfVectorizer()),
+        "xg_boost": XgBoost(models_path),
         # Add other model instances here
     }
-    models_path = "../../../models/sentiment_analysis"
+
     # Train the models and save them
     train_models(models, X_train, y_train, models_path)
-    best_model, best_vectorizer, best_dim_reducer, best_model_name, best_accuracy = find_best_model(
-        models_path, X_test, y_test
-    )
+    best_model, best_model_name, best_accuracy = find_best_model(models, models_path, X_test, y_test)
     print(f"Best model: {best_model_name}")
     print(f"Accuracy: {best_accuracy}")
--- src/models/sentiment_analysis/xg_boost.py
+++ src/models/sentiment_analysis/xg_boost.py
@@ -1,3 +1,7 @@
+import os
+import pickle
+
+import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from xgboost import XGBClassifier
 
@@ -5,10 +9,52 @@ from src.models.sentiment_analysis.base_model import BaseModel
 
 
 class XgBoost(BaseModel):
-    def __init__(self, vectorizer, dim_reducer=None, *args, **kwargs):
-        super().__init__(vectorizer=vectorizer, dim_reducer=dim_reducer, *args, **kwargs)
-        self.model = XGBClassifier(use_label_encoder=False, eval_metric="mlogloss")
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.vectorizer = TfidfVectorizer()
+        self.model = XGBClassifier(use_label_encoder=False, eval_metric="mlogloss", random_state=4265)
 
     def fit(self, X_train, y_train):
-        X_train_processed = self.process(X_train)
-        self.model.fit(X_train_processed, y_train)
+        # X_train_processed = self.fit_transform(X_train)
+
+        vectorizer = self.vectorizer
+        X_train.reset_index(drop=True, inplace=True)
+        X_train_tfidf = vectorizer.fit_transform(X_train["cleaned_text"])
+        X_train_tfidf = pd.DataFrame(X_train_tfidf.toarray(), columns=vectorizer.get_feature_names_out())
+        X_train_clean = X_train.drop(["cleaned_text", "text"], axis=1)
+        X_train_concat = pd.concat([X_train_clean, X_train_tfidf], axis=1)
+        X_train_concat = X_train_concat.loc[:, ~X_train_concat.columns.duplicated()].copy()
+
+        self.model.fit(X_train_concat, y_train)
+
+    def save(self, model_name):
+        # Save the model, vectorizer to pickle files
+        self.model_dir = os.path.join(self.models_path, model_name)
+
+        with open(os.path.join(self.model_dir, "model.pkl"), "wb") as f:
+            pickle.dump(self.model, f)
+        with open(os.path.join(self.model_dir, "vectorizer.pkl"), "wb") as f:
+            pickle.dump(self.vectorizer, f)
+
+    def load(self, model_name):
+        # Load model, vectorizer
+        self.model_dir = os.path.join(self.models_path, model_name)
+
+        with open(os.path.join(self.model_dir, "model.pkl"), "rb") as f:
+            self.model = pickle.load(f)
+        with open(os.path.join(self.model_dir, "vectorizer.pkl"), "rb") as f:
+            self.vectorizer = pickle.load(f)
+
+    def predict(self, X_test):
+        vectorizer = self.vectorizer
+        X_test.reset_index(drop=True, inplace=True)
+        X_test_tfidf = vectorizer.transform(X_test["cleaned_text"])
+        X_test_tfidf = pd.DataFrame(X_test_tfidf.toarray(), columns=vectorizer.get_feature_names_out())
+        X_test_clean = X_test.drop(["cleaned_text", "text"], axis=1)
+        X_test_concat = pd.concat([X_test_clean, X_test_tfidf], axis=1)
+        X_test_concat = X_test_concat.loc[:, ~X_test_concat.columns.duplicated()].copy()
+        probability = self.model.predict_proba(X_test_concat)[:, 0].tolist()  # probability of negative sentiment
+        results = [0 if prob > 0.5 else 1 for prob in probability]
+        probability = [prob if prob > 0.5 else 1 - prob for prob in probability]
+
+        return {"predicted_sentiment": results, "predicted_sentiment_probability": probability}
