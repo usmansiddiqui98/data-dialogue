81c0d102413619feaeabf9cc2bbcf0d1b5beec80
chae-seong <chaeseong123@hotmail.com>
2023-04-09 18:38:06 +0800

    Update LDA and LSA get_topic method to return dictionary

--- src/models/topic_modelling/LDA.py
+++ src/models/topic_modelling/LDA.py
@@ -5,8 +5,9 @@ from nltk.tokenize import word_tokenize
 
 
 class LDAGensim:
-    def __init__(self, df, tags=None):
+    def __init__(self, df, num_topics=10, tags=None):
         self.df = df
+        self.num_topics = num_topics
         self.tags = tags
         self.id2word = None
         self.corpus = None
@@ -21,7 +22,7 @@ class LDAGensim:
             output.append([token.lemma_ for token in doc if token.pos_ in tags])
         return output
 
-    def print_topics(self, num_topics, passes=10):
+    def get_topics(self, passes=10):
         if not self.tags:
             reviews = [word_tokenize(review) for review in self.df["cleaned_text"]]
         else:
@@ -29,8 +30,12 @@ class LDAGensim:
         self.id2word = corpora.Dictionary(reviews)
         self.corpus = [self.id2word.doc2bow(tokens) for tokens in reviews]
         self.lda_model = gensim.models.ldamodel.LdaModel(
-            corpus=self.corpus, id2word=self.id2word, num_topics=num_topics, passes=passes, random_state=4263
+            corpus=self.corpus, id2word=self.id2word, num_topics=self.num_topics, passes=passes, random_state=4263
         )
-        for element in self.lda_model.print_topics():
-            print("Topic " + str(element[0]))
-            print(element[1])
+        topics = {}
+        for topic, words in self.lda_model.show_topics(num_topics=self.num_topics, formatted=False):
+            topics[topic] = {}
+            for word, weight in words:
+                topics[topic][word] = weight
+
+        return topics
--- src/models/topic_modelling/LSA.py
+++ src/models/topic_modelling/LSA.py
@@ -5,8 +5,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 class LSAModel:
-    def __init__(self, df, tags=None):
+    def __init__(self, df, num_topics=10, tags=None):
         self.df = df
+        self.num_topics = num_topics
         self.tags = tags
         self.vectorizer = None
         self.X = None
@@ -22,7 +23,7 @@ class LSAModel:
             output.append([token.lemma_ for token in doc if token.pos_ in tags])
         return output
 
-    def print_topics(self, num_topics):
+    def get_topics(self):
         if not self.tags:
             reviews = [word_tokenize(review) for review in self.df["cleaned_text"]]
         else:
@@ -31,12 +32,19 @@ class LSAModel:
         reviews = [item for sublist in reviews for item in sublist]
         self.vectorizer = TfidfVectorizer(stop_words="english", max_features=1000, max_df=0.5, smooth_idf=True)
         self.X = self.vectorizer.fit_transform(reviews)
-        self.svd_model = TruncatedSVD(n_components=num_topics, algorithm="randomized", n_iter=100, random_state=122)
+        self.svd_model = TruncatedSVD(
+            n_components=self.num_topics, algorithm="randomized", n_iter=100, random_state=122
+        )
         self.svd_model.fit(self.X)
         self.terms = self.vectorizer.get_feature_names_out()
 
+        topics = {}
         for i, comp in enumerate(self.svd_model.components_):
+            topic = {}
             terms_comp = zip(self.terms, comp)
-            sorted_terms = sorted(terms_comp, key=lambda x: x[1], reverse=True)[:num_topics]
-            print("Topic " + str(i) + ": ")
-            print(sorted_terms)
+            sorted_terms = sorted(terms_comp, key=lambda x: x[1], reverse=True)[: self.num_topics]
+            for term, weight in sorted_terms:
+                topic[term] = weight
+            topics[i] = topic
+
+        return topics
