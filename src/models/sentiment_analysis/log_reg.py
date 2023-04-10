c49b9b9a6b69416e850fedfcb5fa68c46ff63ea9
usmansiddiqui98 <usmanhasansiddiqui@gmail.com>
2023-04-10 18:40:01 +0800

    add standard scalar to log reg

--- src/models/sentiment_analysis/log_reg.py
+++ src/models/sentiment_analysis/log_reg.py
@@ -4,6 +4,7 @@ import pickle
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 
 from src.models.sentiment_analysis.base_model import BaseModel
 
@@ -12,6 +13,7 @@ class LogReg(BaseModel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.vectorizer = TfidfVectorizer()
+        self.scaler = StandardScaler()
         self.model = LogisticRegression(random_state=4265, max_iter=1000)
 
     def fit(self, X_train, y_train):
@@ -20,8 +22,8 @@ class LogReg(BaseModel):
         X_train_clean = X_train.drop(["cleaned_text", "text"], axis=1)
         X_train_concat = pd.concat([X_train_clean, X_train_bow], axis=1)
         X_train_concat = X_train_concat.loc[:, ~X_train_concat.columns.duplicated()].copy()
-
-        self.model.fit(X_train_concat, y_train)
+        X_train_scaled = self.scaler.fit_transform(X_train_concat)
+        self.model.fit(X_train_scaled, y_train)
 
     def save(self, model_name):
         self.model_dir = os.path.join(self.models_path, model_name)
@@ -32,6 +34,9 @@ class LogReg(BaseModel):
         with open(os.path.join(self.model_dir, "vectorizer.pkl"), "wb") as f:
             pickle.dump(self.vectorizer, f)
 
+        with open(os.path.join(self.model_dir, "scaler.pkl"), "wb") as f:
+            pickle.dump(self.scaler, f)
+
     def load(self, model_name):
         self.model_dir = os.path.join(self.models_path, model_name)
 
@@ -41,13 +46,18 @@ class LogReg(BaseModel):
         with open(os.path.join(self.model_dir, "vectorizer.pkl"), "rb") as f:
             self.vectorizer = pickle.load(f)
 
+        with open(os.path.join(self.model_dir, "scaler.pkl"), "rb") as f:
+            self.scaler = pickle.load(f)
+
     def predict(self, X_test):
         X_test_bow = self.vectorizer.transform(X_test["cleaned_text"])
         X_test_bow = pd.DataFrame(X_test_bow.toarray(), columns=self.vectorizer.get_feature_names_out())
         X_test_clean = X_test.drop(["cleaned_text", "text"], axis=1)
         X_test_concat = pd.concat([X_test_clean, X_test_bow], axis=1)
         X_test_concat = X_test_concat.loc[:, ~X_test_concat.columns.duplicated()].copy()
-        probability = self.model.predict_proba(X_test_concat)[:, 0].tolist()  # probability of negative sentiment
+
+        X_test_scaled = self.scaler.transform(X_test_concat)
+        probability = self.model.predict_proba(X_test_scaled)[:, 0].tolist()  # probability of negative sentiment
         results = [0 if prob > 0.5 else 1 for prob in probability]
         probability = [prob if prob > 0.5 else 1 - prob for prob in probability]
         return {"predicted_sentiment": results, "predicted_sentiment_probability": probability}
