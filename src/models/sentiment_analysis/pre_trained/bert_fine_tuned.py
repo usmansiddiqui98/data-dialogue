54db25c5bfbe56f80490a9d0106af6f01e6e999e
jialin-low <73953587+jialin-low@users.noreply.github.com>
2023-04-12 15:44:27 +0800

    Update bert_fine_tuned.py

--- src/models/sentiment_analysis/pre_trained/bert_fine_tuned.py
+++ src/models/sentiment_analysis/pre_trained/bert_fine_tuned.py
@@ -1,131 +1,94 @@
 # Import necessary libraries
-# Misc.
-import warnings
-from collections import defaultdict
-from textwrap import wrap
-
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import seaborn as sns
 import torch
-
-# Torch ML libraries
-import transformers
-from matplotlib import rc
-from pylab import rcParams
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.model_selection import train_test_split
-from torch import nn, optim
-from torch.utils.data import DataLoader, Dataset
+from torch import nn
+from torch.utils.data import DataLoader
+from src.models.sentiment_analysis.bert import BERTDataset
 
 # Load Saved Model
 from transformers import (
-    AdamW,
-    BertConfig,
-    BertForSequenceClassification,
     BertModel,
-    BertTokenizer,
-    get_linear_schedule_with_warmup,
+    BertTokenizer
 )
-
-# warnings.filterwarnings('ignore')
 
 
 class BertFineTuned:
     def __init__(self, path):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.PATH = path
-
-    def get_predictions(self, model, data_loader):
-        model = model.eval()
-        review_texts = []
-        predictions = []
-        prediction_probs = []
-        real_values = []
-
-        with torch.no_grad():
-            for d in data_loader:
-                texts = d["review_text"]
-                input_ids = d["input_ids"].to(self.device)
-                attention_mask = d["attention_mask"].to(self.device)
-                targets = d["targets"].to(self.device)
-
-                # Get outputs
-                outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-                _, preds = torch.max(outputs, dim=1)
-
-                review_texts.extend(texts)
-                predictions.extend(preds)
-                prediction_probs.extend(outputs)
-                real_values.extend(targets)
-
-        predictions = torch.stack(predictions).cpu()
-        prediction_probs = torch.stack(prediction_probs).cpu()
-        real_values = torch.stack(real_values).cpu()
-
-        return review_texts, predictions, prediction_probs, real_values
+        self.tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
+        self.saved_model = None
 
     def predict(self, x_test):
         # Build a BERT based tokenizer
-        tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
-
-        test_data_loader = create_data_loader(x_test, tokenizer, 200, 16)
+        test_encodings = self.tokenizer(x_test, truncation=True, padding=True, max_length=256, return_tensors="pt")
+        test_dataset = BERTDataset(test_encodings, np.zeros(len(x_test)))
+        test_dataloader = DataLoader(test_dataset, batch_size=16, num_workers=0)
 
         class_names = ["0", "1"]
-        saved_model = SentimentClassifier(len(class_names))
-        saved_model.load_state_dict(torch.load(self.PATH, map_location=self.device))
-        saved_model = saved_model.to(self.device)
+        self.saved_model = SentimentClassifier(len(class_names))
+        self.saved_model.load_state_dict(torch.load(self.PATH, map_location=self.device))
+        self.saved_model = self.saved_model.to(self.device)
 
-        y_review_texts, y_pred, y_pred_probs, y_test = self.get_predictions(saved_model, test_data_loader)
-        return y_pred
+        self.saved_model.eval()
+        predictions = []
 
+        with torch.no_grad():
+            for batch in test_dataloader:
+                inputs = {key: value.to(self.device) for key, value in batch.items() if key != "labels"}
+                outputs = self.saved_model(**inputs)
+                logits = outputs.logits
+                preds = torch.argmax(logits, dim=1).cpu().numpy()
+                predictions.extend(preds)
 
-class GPReviewDataset(Dataset):
-    # Constructor Function
-    def __init__(self, reviews, targets, tokenizer, max_len):
-        self.reviews = reviews
-        self.targets = targets
-        self.tokenizer = tokenizer
-        self.max_len = max_len
-
-    # Length magic method
-    def __len__(self):
-        return len(self.reviews)
-
-    # get item magic method
-    def __getitem__(self, item):
-        review = str(self.reviews[item])
-        target = self.targets[item]
-
-        # Encoded format to be returned
-        encoding = self.tokenizer.encode_plus(
-            review,
-            add_special_tokens=True,
-            max_length=self.max_len,
-            return_token_type_ids=False,
-            pad_to_max_length=True,
-            return_attention_mask=True,
-            return_tensors="pt",
-        )
-
-        return {
-            "review_text": review,
-            "input_ids": encoding["input_ids"].flatten(),
-            "attention_mask": encoding["attention_mask"].flatten(),
-            "targets": torch.tensor(target, dtype=torch.long),
-        }
+        return predictions
 
 
-def create_data_loader(df, tokenizer, max_len, batch_size):
-    ds = GPReviewDataset(
-        reviews=df.cleaned_text.to_numpy(),
-        # targets=df.Sentiment.to_numpy(),
-        tokenizer=tokenizer,
-        max_len=max_len,
-    )
-
-    return DataLoader(ds, batch_size=batch_size, num_workers=0)
+# class GPReviewDataset(Dataset):
+#     # Constructor Function
+#     def __init__(self, reviews, targets, tokenizer, max_len):
+#         self.reviews = reviews
+#         self.targets = targets
+#         self.tokenizer = tokenizer
+#         self.max_len = max_len
+#
+#     # Length magic method
+#     def __len__(self):
+#         return len(self.reviews)
+#
+#     # get item magic method
+#     def __getitem__(self, item):
+#         review = str(self.reviews[item])
+#         target = self.targets[item]
+#
+#         # Encoded format to be returned
+#         encoding = self.tokenizer.encode_plus(
+#             review,
+#             add_special_tokens=True,
+#             max_length=self.max_len,
+#             return_token_type_ids=False,
+#             pad_to_max_length=True,
+#             return_attention_mask=True,
+#             return_tensors="pt",
+#         )
+#
+#         return {
+#             "review_text": review,
+#             "input_ids": encoding["input_ids"].flatten(),
+#             "attention_mask": encoding["attention_mask"].flatten(),
+#             "targets": torch.tensor(target, dtype=torch.long),
+#         }
+#
+#
+# def create_data_loader(df, tokenizer, max_len, batch_size):
+#     ds = GPReviewDataset(
+#         reviews=df.cleaned_text.to_numpy(),
+#         # targets=df.Sentiment.to_numpy(),
+#         tokenizer=tokenizer,
+#         max_len=max_len,
+#     )
+#
+#     return DataLoader(ds, batch_size=batch_size, num_workers=0)
 
 
 class SentimentClassifier(nn.Module):
