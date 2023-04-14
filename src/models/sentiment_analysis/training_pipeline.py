75eb8196ee7a8d57a7a94dc650d660d220f0a5ff
jialin-low <73953587+jialin-low@users.noreply.github.com>
2023-04-14 23:04:02 +0800

    Update training_pipeline.py

--- src/models/sentiment_analysis/training_pipeline.py
+++ src/models/sentiment_analysis/training_pipeline.py
@@ -120,14 +120,14 @@ if __name__ == "__main__":
     else:
         models_path = os.path.join(BASE_DIR, "models/sentiment_analysis")
     models = {
-        # "xg_boost": XgBoost(models_path),
-        # "xg_boost_svd": XgBoostSvd(models_path),
-        # "log_reg": LogReg(models_path),
+        "xg_boost": XgBoost(models_path),
+        "xg_boost_svd": XgBoostSvd(models_path),
+        "log_reg": LogReg(models_path),
         "svm": SVM(models_path),
         "naive_bayes": Naivebayes(models_path),
-        # "bert_fine_tuned": BertFineTuned(models_path),
-        # "siebert": Siebert(models_path),
-        # "lstm": BasicLSTM(models_path),
+        "bert_fine_tuned": BertFineTuned(models_path),
+        "siebert": Siebert(models_path),
+        "lstm": BasicLSTM(models_path),
     }
 
     # Train the models and save them
